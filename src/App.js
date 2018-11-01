c1b0ed78737f2f344a8c60ea66d5413781078098
Chandon <chandonking@hotmail.com>
2018-11-01 15:11:38 +0000

    Added 6 tv show titles task #3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-import movieCover from './images/pif.jpg';
+import blackmirror from './images/1.jpg';
+import breakingbad from './images/2.jpg';
+import deathnote from './images/3.jpg';
+import got from './images/4.jpg';
+import fowd from './images/5.jpg';
+import wire from './images/6.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={movieCover} className="movieCover" alt="paid in full" />
+        <div class="row">
+         <div className="column">
+         <h2>Black Mirror</h2>
+         <img src={blackmirror} alt="" />
+         </div> 
+         <div class="column">
+         <h2>Breaking Bad</h2>
+         <img src={breakingbad} alt="" />
+         </div>
+         <div class="column">
+         <h2>Death Note</h2>
+         <img src={deathnote} alt="death note" />
+         </div>
+        </div>
+
+        <div class="row">
+         <div class="column">
+         <h2>Got</h2>
+         <img src={got} alt="Got" />>
+         </div> 
+         <div class="column">
+         <h2>Fear The Walking Dead</h2>
+         <img src={fowd} alt="Fear of The Walking Dead" />
+         </div>
+         <div class="column">
+         <h2>The Wire</h2>
+         <img src={wire} alt="The Wire" />
+         </div>
+        </div> 
+
       </div>
+      
     );
   }
 }
