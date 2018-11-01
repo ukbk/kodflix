daf0e0c2cacc6e9cb83044cfcfe957905c8aea33
Chandon <chandonking@hotmail.com>
2018-11-01 11:49:26 +0000

    Added a movie vover

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import movieCover from './images/pif.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React in July again</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={movieCover} className="MovieCover" alt="paid in full" />
       </div>
     );
   }
