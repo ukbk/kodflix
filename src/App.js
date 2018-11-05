79bc4caa409460fcc45d6a2469b085c0867a39a3
Chandon <chandonking@hotmail.com>
2018-11-05 14:01:12 +0000

    Added an overlay to Show movie title on cover when hovering Task #6

--- src/App.js
+++ src/App.js
@@ -11,40 +11,56 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="row">
-         <div className="column">
-         <h2>Black Mirror</h2>
-         <img src={blackmirror} alt="" />
-         </div> 
-         <div class="column">
-         <h2>Breaking Bad</h2>
-         <img src={breakingbad} alt="" />
-         </div>
-         <div class="column">
-         <h2>Death Note</h2>
-         <img src={deathnote} alt="death note" />
-         </div>
-        </div>
+        <div className="row">
 
-        <div class="row">
-         <div class="column">
-         <h2>Got</h2>
-         <img src={got} alt="Got" />>
-         </div> 
-         <div class="column">
-         <h2>Fear The Walking Dead</h2>
-         <img src={fowd} alt="Fear of The Walking Dead" />
-         </div>
-         <div class="column">
-         <h2>The Wire</h2>
-         <img src={wire} alt="The Wire" />
-         </div>
-        </div> 
+<div className="item">
+  <img className="column" src={blackmirror} alt="" />
+  <div className="overlay">
+    <div className="text">Black Mirror</div>
+  </div>
+</div>
 
-      </div>
-      
-    );
-  }
+<div className="item">
+  <img className="column" src={breakingbad} alt="" />
+  <div className="overlay">
+    <div className="text">Breaking Bad</div>
+  </div>
+</div>
+
+<div className="item">
+  <img className="column" src={deathnote} alt="" />
+  <div className="overlay">
+    <div className="text">Death Note</div>
+  </div>
+</div>
+</div>
+
+<div className="row">
+<div className="item">
+  <img className="column" src={got} alt="" />
+  <div className="overlay">
+    <div className="text">Got</div>
+  </div>
+</div>
+
+<div className="item">
+  <img className="column" src={fowd} alt="" />
+  <div className="overlay">
+    <div className="text">Fear of The Walking Dead</div>
+  </div>
+</div>
+
+<div className="item">
+  <img className="column" src={wire} alt="" />
+  <div className="overlay">
+    <div className="text">The Wire</div>
+  </div>
+</div>
+</div>
+
+</div>
+);
+}
 }
 
 export default App;
